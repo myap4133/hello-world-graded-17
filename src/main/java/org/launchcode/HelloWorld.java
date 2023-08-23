e9e3536d92c7a082acdd16772f1119d21570b0f5
may2fn <may2fn@umsystem.edu>
2023-08-23 13:22:42 -0500

    Changed the Hello World

---
+++ src/main/java/org/launchcode/HelloWorld.java
@@ -0,0 +1,11 @@
+package org.launchcode;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println(sayHello());
+    }
+
+    public static String sayHello() {
+        return "Hello, World!";
+    }
+}
